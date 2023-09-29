cc4bab174ff0f19c6c995a484736acb0461838d9
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-09-29 20:31:56 +0700

    my reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+import MenuForm from './components/MenuForm'
+import SignUpForm from './components/SignUpForm'
+import LoginForm from './components/LoginForm'
+import MyApp from './components/MyApp'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* <Header/> */}
+      <MyApp/>
+      <SignUpForm/>
+      <div>App</div>
+      <LoginForm/>
+      <MenuForm/>
+      <Footer/>
+    </>
+  )
 }
-
-export default App;
