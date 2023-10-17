66cf9488d89fb5f121e26b4b0235dce88dd6bead
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-10-18 06:10:56 +0700

    feedback by input

--- src/component/page/ListBranch/ListBranch.jsx
+++ src/component/page/ListBranch/ListBranch.jsx
@@ -4,12 +4,17 @@ import './ListBranch.scss';
 import StarRating from '../../Context/Rating';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
-
-
+import Cookies from 'js-cookie';
+import jwt_decode from 'jwt-decode';
+import Rating from 'react-rating';
 const ListBranch = () => {
     const [branches, setBranches] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [feedbackData, setFeedbackData] = useState([]);
+    const [userFeedback, setUserFeedback] = useState('');
+    const [content, setContent] = useState('');
+    const [rating, setRating] = useState(0);
+    const [branchId, setBranchId] = useState(0);
     const openModal = (branchId) => {
         // Gọi hàm fetchFeedbacksByBranch để tải dữ liệu phản hồi và lưu vào feedbackData state.
         fetchFeedbacksByBranch(branchId);
@@ -24,7 +29,6 @@ const ListBranch = () => {
             minute: 'numeric',
             second: 'numeric',
         };
-
         const formattedDate = new Date(dateString).toLocaleString(undefined, options);
         return formattedDate;
     }
@@ -52,26 +56,62 @@ const ListBranch = () => {
         const fullStars = Math.floor(rating);
         const halfStars = Math.ceil(rating - fullStars);
         const emptyStars = 5 - fullStars - halfStars;
-      
+
         const starArray = [];
-      
+
         // Add full stars with a yellow color
         for (let i = 0; i < fullStars; i++) {
-          starArray.push(<FontAwesomeIcon key={i} icon={faStar} style={{ color: 'yellow' }} />);
+            starArray.push(<FontAwesomeIcon key={i} icon={faStar} style={{ color: 'gold' }} />);
         }
-      
+
         // Add half stars with a yellow color
         if (halfStars === 1) {
-          starArray.push(<FontAwesomeIcon key="half" icon={faStarHalfAlt} style={{ color: 'yellow' }} />);
+            starArray.push(<FontAwesomeIcon key="half" icon={faStarHalfAlt} style={{ color: 'gold' }} />);
         }
-      
+
         // Add empty stars with a gray color
         for (let i = 0; i < emptyStars; i++) {
-          starArray.push(<FontAwesomeIcon key={`empty${i}`} icon={faStar} style={{ color: 'gray' }} />);
+            starArray.push(<FontAwesomeIcon key={`empty${i}`} icon={faStar} style={{ color: 'gray' }} />);
         }
-      
+
         return starArray;
-      };
+    };
+    const tokenFromCookie = Cookies.get('token_user');
+    let id = null;
+    if (tokenFromCookie) {
+        const decodedToken = jwt_decode(tokenFromCookie);
+        id = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
+    }
+    const submitFeedback = () => {
+        console.log(id);
+    
+        const feedbackData = {
+            userId: id, // Lấy userId từ token
+            content: content, // Lấy content từ state
+            rating: rating, // Lấy rating từ state
+            branchId: branchId, // Lấy branchId từ state
+        };
+    
+        fetch('https://localhost:7296/api/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${tokenFromCookie}`
+            },
+            body: JSON.stringify(feedbackData),
+        })
+        .then((response) => {
+            if (response.ok) {
+                console.log('Phản hồi đã được tạo thành công');
+                // Đóng modal hoặc làm điều gì đó khác sau khi gửi phản hồi thành công
+                setShowModal(false);
+            } else {
+                console.error('Lỗi khi tạo phản hồi');
+            }
+        });
+    };
+    
+    
     return (
         <>
             <Row className='branch'>
@@ -82,6 +122,7 @@ const ListBranch = () => {
                                 <Card.Img variant="top" src={branch.image} className="fixed-height-image" />
                             </div>
                             <Card.Body>
+                                <Card.Title>{branch.name}</Card.Title>
                                 <Card.Title>{branch.description}</Card.Title>
                                 <Card.Title>{branch.address}</Card.Title>
                                 <Card.Title>{branch.phone}</Card.Title>
@@ -102,30 +143,47 @@ const ListBranch = () => {
                     <Modal.Title>Danh sách phản hồi</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {feedbackData.map((feedback) => (
-                        <div key={feedback.feedbackId} className="feedback-item">
-                            <Card.Img className="avatar" variant="top" src={feedback.id.avatar} />
-                            <div className="email-info">
-                                <p className="email">{feedback.id.email}</p>
-                                <p className="content">{feedback.content}</p>
-                                <p className="rating">{renderStarRating(feedback.rating)}</p>
+                    <div className="feedback-list-container">
+                        {feedbackData.map((feedback) => (
+                            <div key={feedback.feedbackId} className="feedback-item">
+                                <Card.Img className="avatar" variant="top" src={feedback.id.avatar} />
+                                <div className="email-info">
+                                    <p className="email">{feedback.id.email}</p>
+                                    <p className="content">{feedback.content}</p>
+                                    <p className="rating">{renderStarRating(feedback.rating)}</p>
+                                </div>
+                                <p className="feedback-date">{formatDate(feedback.feedbackDate)}</p>
+                                {/* Và bất kỳ thông tin nào khác bạn muốn hiển thị */}
                             </div>
-                            <p className="feedback-date">{formatDate(feedback.feedbackDate)}</p>
-                            {/* Và bất kỳ thông tin nào khác bạn muốn hiển thị */}
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+
+                    {/* Vùng nhập phản hồi */}
+                    <div className="feedback-input">
+                        <h4>Nhập phản hồi của bạn</h4>
+                        <textarea
+    placeholder="Nhập phản hồi của bạn..."
+    rows="4"
+    cols="50"
+    value={content}
+    onChange={(e) => setContent(e.target.value)}
+></textarea>
+<input
+    type="number"
+    placeholder="Điểm đánh giá"
+    value={rating}
+    onChange={(e) => setRating(e.target.value)}
+/>
+                        <button onClick={submitFeedback}>Gửi phản hồi</button>
+                    </div>
                 </Modal.Body>
+
                 <Modal.Footer>
                     <Button variant="secondary" onClick={() => setShowModal(false)}>
                         Đóng
                     </Button>
                 </Modal.Footer>
             </Modal>
-
-
-
-
-
 
         </>
     );
